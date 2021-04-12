f329388eb80e6cda70456e756e54df2e09fe00f4
Roberth-Lopez <81263698+Roberth-Lopez@users.noreply.github.com>
2021-04-12 13:23:10 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -120,34 +120,8 @@ function enviar(){
   function onMessageArrived(message) {
     console.log("onMessageArrived:"+message.payloadString);
     Mensaje=message.payloadString;
-	if((message.destinationName == "ralopez.fie@unach.edu.ec/test1") && (Mensaje == "Led 1 Encendido") ){
-	   document.getElementById("sensor1").innerHTML=message.payloadString;
-	   }
-	else if((message.destinationName == "ralopez.fie@unach.edu.ec/test1") && (Mensaje == "Led 2 Encendido") ){
-	   document.getElementById("sensor2").innerHTML=message.payloadString;
-	   }
-	else if((message.destinationName == "ralopez.fie@unach.edu.ec/test1") && (Mensaje == "Led 1 Apagado") ){
-	   document.getElementById("sensor1").innerHTML=message.payloadString;
-	   }
-	else if((message.destinationName == "ralopez.fie@unach.edu.ec/test1") && (Mensaje == "Led 2 Apagado") ){
-	   document.getElementById("sensor2").innerHTML=message.payloadString;
-	   }  
-	else if(message.destinationName == "ralopez.fie@unach.edu.ec/test2"){
-	  document.getElementById("historial").innerHTML=message.payloadString;
-	}
-	else if((message.destinationName == "ralopez.fie@unach.edu.ec/test3") && (Mensaje == "Led 1 Encendido") ){
-	  document.getElementById("estado1").innerHTML=message.payloadString;
-	}
-	else if((message.destinationName == "ralopez.fie@unach.edu.ec/test3") && (Mensaje == "Led 2 Encendido") ){
-	  document.getElementById("estado2").innerHTML=message.payloadString;
-	}  
-	else if((message.destinationName == "ralopez.fie@unach.edu.ec/test3") && (Mensaje == "Led 1 Apagado") ){
-	  document.getElementById("estado1").innerHTML=message.payloadString;
-	}  
-	else if((message.destinationName == "ralopez.fie@unach.edu.ec/test3") && (Mensaje == "Led 2 Apagado") ){
-	  document.getElementById("estado2").innerHTML=message.payloadString;
-	}
-	else if(message.destinationName == "ralopez.fie@unach.edu.ec/test5"){
+	
+	if(message.destinationName == "ralopez.fie@unach.edu.ec/test5"){
 	  document.getElementById("hora").innerHTML=message.split("")[0];
 	  document.getElementById("minuto").innerHTML=message.split("")[2];
 	  document.getElementById("segundo").innerHTML=message.split("")[4];	
