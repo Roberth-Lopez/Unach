59e0a37e6754242038e1f0b481785cfb50a26161
Roberth-Lopez <81263698+Roberth-Lopez@users.noreply.github.com>
2021-04-12 13:27:46 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -122,9 +122,9 @@ function enviar(){
     Mensaje=message.payloadString;
 	
 	if(message.destinationName == "ralopez.fie@unach.edu.ec/test5"){
-	  document.getElementById("hora").innerHTML=message.split("")[0];
-	  document.getElementById("minuto").innerHTML=message.split("")[2];
-	  document.getElementById("segundo").innerHTML=message.split("")[4];	
+	  document.getElementById("hora").innerHTML=Mensaje.split("")[0];
+	  document.getElementById("minuto").innerHTML=Mensaje.split("")[2];
+	  document.getElementById("segundo").innerHTML=Mensaje.split("")[4];	
 	}    
   }
   
